1a47645dbf032d0643f606f863d370cb30f4272c
Nhuan <nhuan.hoangduc@gmail.com>
2017-07-25 15:46:06 +0700

    Write log to file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -16,7 +17,10 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+var logStream = fs.createWriteStream(__dirname + '/logs', { flags: 'a' });
+app.use(logger('combined', {
+  stream: logStream
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
